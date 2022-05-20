4a53f32134985f95b2fc4a291170b6c5217badd6
sergei <syagelo@gmail.com>
2022-05-20 19:30:03 +0300

    day-28

---
+++ day-28/main.py
@@ -0,0 +1,46 @@
+from email.mime import image
+from tkinter import *
+from turtle import bgcolor, color
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title('Pomodoro')
+window.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)  
+tomato_img = PhotoImage(file='day-28/tomato.png')
+canvas.create_image(101, 112, image=tomato_img)
+canvas.create_text(100, 130, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
+canvas.grid(column=1, row=1)
+
+
+timer_label = Label(text='Timer', font=('Arial', 40),background=YELLOW, fg=GREEN)
+timer_label.grid(column=1, row=0)
+
+check_mark = Label(text='✔',fg=GREEN, font=(40), background=YELLOW)
+check_mark.grid(column=1, row=3)
+
+start_button = Button(text='Start')
+start_button.grid(column=0, row=3)
+
+reset_button = Button(text='Reset')
+reset_button.grid(column=2, row=3)
+
+
+window.mainloop()
